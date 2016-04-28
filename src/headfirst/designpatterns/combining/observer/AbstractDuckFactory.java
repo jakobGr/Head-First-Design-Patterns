2a4201321d62edad4f5385c20987718d04d4824a
jgrieshofer <jakob_g@hotmail.com>
2016-04-28 11:03:21 +0200

    Test Commit

--- src/headfirst/designpatterns/combining/observer/AbstractDuckFactory.java
+++ src/headfirst/designpatterns/combining/observer/AbstractDuckFactory.java
@@ -3,6 +3,7 @@ package headfirst.designpatterns.combining.observer;
 public abstract class AbstractDuckFactory {
  
 	public abstract Quackable createMallardDuck();
+	hfiashfdöoaskdhfö;
 	public abstract Quackable createRedheadDuck();
 	public abstract Quackable createDuckCall();
 	public abstract Quackable createRubberDuck();
